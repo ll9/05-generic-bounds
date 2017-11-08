33e20d18db64249b9245635682d22f23bdef334f
ll9 <llwierer@web.de>
2017-11-08 09:59:13 +0100

    implemented Plant class

--- src/main/java/de/fhro/inf/prg3/a05/model/Plant.java
+++ src/main/java/de/fhro/inf/prg3/a05/model/Plant.java
@@ -1,4 +1,27 @@
 package de.fhro.inf.prg3.a05.model;
 
-public class Plant {
+public abstract class Plant {
+    private double height;
+    private String family;
+    private String name;
+
+    public Plant(double height, String family, String name) {
+        this.height = height;
+        this.family = family;
+        this.name = name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract PlantColor getColor();
 }
