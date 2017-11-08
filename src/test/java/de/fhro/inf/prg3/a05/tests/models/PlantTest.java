d5e21749bcfeea288fb34a9ff22a54565a387f8f
ll9 <llwierer@web.de>
2017-11-08 10:31:33 +0100

    added some tests to flower

--- src/test/java/de/fhro/inf/prg3/a05/tests/models/PlantTest.java
+++ src/test/java/de/fhro/inf/prg3/a05/tests/models/PlantTest.java
@@ -1,5 +1,35 @@
+package de.fhro.inf.prg3.a05.tests.models;
+
+import de.fhro.inf.prg3.a05.model.Flower;
+import de.fhro.inf.prg3.a05.model.PlantColor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlantTest {
+    Flower flower = new Flower(1.5, "Rose", "Rose", PlantColor.RED);
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getHeight() {
+        assertEquals(1.5, flower.getHeight(), 0.001);
+    }
+
+    @Test
+    void getFamily() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getColor() {
+        assertThrows(IllegalArgumentException.class, () -> new Flower(1.5, "Rose", "Rose", PlantColor.GREEN));
+    }
 
 }
